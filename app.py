88bf78289a1233a8f47c4a11588604c57527475f
Ahmed Elakeed <57276446+Ahmed-Elakeed@users.noreply.github.com>
2023-05-22 18:36:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,61 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model
 import pandas as pd
 
-model=load_model("dence_lstm512.h5")
+model2=load_model("best_model(2)(1).h5")
 UPLOAD_FOLDER = 'folders/'
 app = Flask(__name__)
+def Ecgpreprocessing(recieveecg):
+        ecgPath = 'folders/ecg.csv'
+        recieveecg.save(ecgPath)
+        ecg = pd.read_csv(ecgPath)
+        ecg = ecg.to_numpy()
+        file_names = ["newww(1).csv"] 
+        n_features = 32 
+        csv_header = ["f"+str(i) for i in range(n_features+1)] 
+        csv_header = ",".join(csv_header)
+        data = []
+        for file in file_names:
+           label = 1
+           print("Processing file " + file)
+           ecg_signal = np.loadtxt(os.path.join(folderPath, file), delimiter=",")
+           for j in range(0, 10 , 10):
+             AR, rho, ref = arburg(ecg_signal, n_features)
+             features = [k.real for k in AR]
+             features.append(rho)
+             features.append(label)
+             data.append(features)
+        c=np.savetxt("test.csv", data, delimiter=",", header=csv_header, comments="")
+        return c
+
+
+
+@app.route('/ecg')
+def Ecgindex():
+    return render_template('indexecg.html')
+
+@app.route('/predictecg', methods=['GET','POST'])
+def Ecgpredict():
+    if request.method == "POST":
+        recieveecg=request.files['ecg']
+        d = preprocessing(recieveecg)
+        firstprediction = model2.predict(d)
+
+    return render_template('indexecg.html', prediction = firstprediction )
+
+@app.route('/EcgAPI', methods=['GET','POST'])
+def EcgAPI():
+    try:
+        if 'ecg' not in request.files:
+            return "Please try again. The file doesn't exist"
+        recieveecg=request.files['ecg']
+        d = preprocessing(recieveecg)
+        firstprediction = model2.predict(d)
+        return jsonify({'prediction': firstprediction})
+    except:
+        return jsonify({'Error': 'Error occur'})
+
+
+model=load_model("dence_lstm512.h5")
 def preprocessing(recieveecg, recieveppg):
         ecgPath = 'folders/ecg.csv'
         ppgPath = 'folders/ppg.csv'
